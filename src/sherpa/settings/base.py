5701fb837a1c553900869ea75e8ff289102242c8
antonioIrizar <antonioirizar@gmail.com>
2023-02-17 19:26:16 +0100

    fix secret key

--- src/sherpa/settings/base.py
+++ src/sherpa/settings/base.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-id9wdoxkbq*g(rr^uz8llj(mt7n3!fthrb5$g3v=uz_&2+07+9"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
